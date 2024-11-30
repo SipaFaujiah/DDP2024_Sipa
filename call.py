b7050550558f40658f31d204e29bdf3e87df7e87
SipaFaujiah <sipafaujiah93@gmail.com>
2024-11-30 23:22:45 +0700

    Tugas10_SipaFaujiah_0110124111

---
+++ call.py
@@ -0,0 +1,24 @@
+import bangundatar
+
+bangundatar.l_persegi(5)
+bangundatar.l_persegi_panjang(10,5)
+bangundatar.l_segitiga(2,8)
+bangundatar.l_lingkaran(1)
+bangundatar.l_jajargenjang(10,15)
+
+import bangunruang
+
+bangunruang.l_balok(4,9,2)
+bangunruang.l_kubus(6)
+bangunruang.l_tabung(8,6)
+bangunruang.l_limas_segitiga(3,5)
+bangunruang.l_prisma_segitiga(2,4)
+
+import hitung
+
+hitung.tambah(7, 2)
+hitung.kurang(9, 4)
+hitung.kali(3, 4)
+hitung.bagi(13, 5)
+hitung.pangkat(6, 2)
+
---
+++ hitung.py
@@ -0,0 +1,21 @@
+import math
+
+def tambah(bil1, bil2):
+    hasil = 7 + 2
+    print("Hasil Tambah Dari",7, "+", 2, "=", hasil)
+
+def kurang(bil1, bil2):
+    hasil = 9 - 4
+    print("Hasil Pengurangan Dari",9, "-", 4, "=", hasil)
+
+def kali(bil1, bil2):
+    hasil = 5 * 5
+    print("Hasil Perkalian Dari",3, "*", 4, "=", hasil)
+
+def bagi(bil1, bil2):
+    hasil = 16 / 4
+    print("Hasil Pembagian Dari",13, "/", 5, "=", hasil)
+
+def pangkat(bil1, bil2):
+    hasil = math.pow(5, 3)
+    print("Hasil Pemangkatan Dari",6, "^", 2, "=", hasil)
